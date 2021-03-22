50392c40953e8b3df9a98830d5e8a75d62022d2c
Ritsuki Goto <develop@mizucoffee.net>
2021-03-23 01:05:15 +0900

    [add] dist dir

---
+++ dist/types/main.d.ts
@@ -0,0 +1,9 @@
+import LiTKansaiLogo from './components/LiTKansaiLogo.vue';
+import LiTKansaiSplash from './components/LiTKansaiSplash.vue';
+import Vue from "vue";
+declare function install(vue: typeof Vue, options?: any): void;
+declare const _default: {
+    install: typeof install;
+};
+export default _default;
+export { LiTKansaiLogo, LiTKansaiSplash };
